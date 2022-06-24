c296b89636eab80fe1ec6543714e722946b54464
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-06-24 16:28:24 +0300

    add bSSA

--- AlgorithmsImpl/bSSA.py
+++ AlgorithmsImpl/bSSA.py
@@ -1,13 +1,14 @@
 from sklearn.decomposition import PCA, FastICA
 
 
-def dataTransformation(X,y):
+def dataTransformation(X):
     _X=PCA().fit_transform(X)
     _X = FastICA().fit_transform(_X)
     return _X
 
-def bSSA(X,y,binarization_threshold):
-    _X = X.to_numpy()
-    _y = y.to_numpy()
+def bSSA(X,y,binarization_threshold=0.6):
+    # A. FEATURE TRANSFORMATIO
+    X = dataTransformation(X)
 
+    # B. BINARY SALP SWARM ALGORITHM
 
