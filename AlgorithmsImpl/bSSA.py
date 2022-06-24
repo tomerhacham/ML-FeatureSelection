b5206985c2ba5c6f8c2d81326f03ba552c664dce
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-06-24 18:28:17 +0300

    clean code

--- AlgorithmsImpl/bSSA.py
+++ AlgorithmsImpl/bSSA.py
@@ -31,10 +31,7 @@ def calculateFitness(salp, X, y):
     pass
 
 def bSSA(X, y, binarization_threshold=0.6, population_size=20, maxIter=70):
-    # A. FEATURE TRANSFORMATIO
     X = dataTransformation(X)
-
-    # B. BINARY SALP SWARM ALGORITHM
     salps = [np.where(default_rng(42).random(X.shape[1]) > binarization_threshold, 1, 0) for _ in
              range(population_size)]  # creates initial population
     Ymax = np.ones(X.shape[1])
@@ -47,3 +44,6 @@ def bSSA(X, y, binarization_threshold=0.6, population_size=20, maxIter=70):
         for j in range(len(salps)):
             newLocation = updateLeader(salps[j], a1) if j == 0 else (salps[j] + salps[j - 1])/2
             salps[j] = np.where(newLocation > binarization_threshold, 1, 0)  # convert them into binary using threshold δ
+
+    salps.sort(key=lambda salp: calculateFitness(salp, X, y), reverse=True)
+    return salps[0].tolist()
