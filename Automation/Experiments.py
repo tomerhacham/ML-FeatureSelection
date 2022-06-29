cb36594957f86788720b77d4de90714f4615f085
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2022-06-29 13:56:45 +0300

    Update Experiments.py

--- Automation/Experiments.py
+++ Automation/Experiments.py
@@ -4,7 +4,8 @@ from sklearnex import patch_sklearn
 
 patch_sklearn()
 from ReliefF import ReliefF
-from sklearn.metrics import accuracy_score, matthews_corrcoef, roc_auc_score, precision_recall_curve, make_scorer
+from sklearn.metrics import accuracy_score, matthews_corrcoef, roc_auc_score, precision_recall_curve, make_scorer, \
+    average_precision_score
 from AlgorithmsImpl.FAST import FAST
 from AlgorithmsImpl.bSSA import bSSA, bSSA__New
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, SelectFdr, RFE
@@ -67,78 +68,6 @@ def load_dataset():
     return X, y
 
 
-def _safe_split(estimator, X, y, indices, train_indices=None):
-    """Create subset of dataset and properly handle kernels.
-
-    Slice X, y according to indices for cross-validation, but take care of
-    precomputed kernel-matrices or pairwise affinities / distances.
-
-    If ``estimator._pairwise is True``, X needs to be square and
-    we slice rows and columns. If ``train_indices`` is not None,
-    we slice rows using ``indices`` (assumed the test set) and columns
-    using ``train_indices``, indicating the training set.
-
-    .. deprecated:: 0.24
-
-        The _pairwise attribute is deprecated in 0.24. From 1.1
-        (renaming of 0.26) and onward, this function will check for the
-        pairwise estimator tag.
-
-    Labels y will always be indexed only along the first axis.
-
-    Parameters
-    ----------
-    estimator : object
-        Estimator to determine whether we should slice only rows or rows and
-        columns.
-
-    X : array-like, sparse matrix or iterable
-        Data to be indexed. If ``estimator._pairwise is True``,
-        this needs to be a square array-like or sparse matrix.
-
-    y : array-like, sparse matrix or iterable
-        Targets to be indexed.
-
-    indices : array of int
-        Rows to select from X and y.
-        If ``estimator._pairwise is True`` and ``train_indices is None``
-        then ``indices`` will also be used to slice columns.
-
-    train_indices : array of int or None, default=None
-        If ``estimator._pairwise is True`` and ``train_indices is not None``,
-        then ``train_indices`` will be use to slice the columns of X.
-
-    Returns
-    -------
-    X_subset : array-like, sparse matrix or list
-        Indexed data.
-
-    y_subset : array-like, sparse matrix or list
-        Indexed targets.
-
-    """
-    if _is_pairwise(estimator):
-        if not hasattr(X, "shape"):
-            raise ValueError("Precomputed kernels or affinity matrices have "
-                             "to be passed as arrays or sparse matrices.")
-        # X is a precomputed square kernel matrix
-        if X.shape[0] != X.shape[1]:
-            raise ValueError("X should be a square kernel matrix")
-        if train_indices is None:
-            X_subset = X[np.ix_(indices, indices)]
-        else:
-            X_subset = X[np.ix_(indices, train_indices)]
-    else:
-        X_subset = _safe_indexing(X, indices)
-
-    if y is not None:
-        y_subset = _safe_indexing(y, indices)
-    else:
-        y_subset = None
-
-    return X_subset, y_subset
-
-
 for dataset in datasets:
     X, y = load_dataset()
     features_names = list(X.columns)
@@ -156,20 +85,22 @@ for dataset in datasets:
                 #  ACC, MCC ,AUC,  PR-AUC. יצוין כי בבעיות Multi-class
                 n_classes = y.nunique(dropna=False)
                 print(f'n_classes:{n_classes}')
-                metrics = {'ACC': make_scorer(accuracy_score),
-                           'MCC': make_scorer(matthews_corrcoef)}
-                # 'PR-AUC':make_scorer(precision_recall_curve)}
-                metrics = {}
+                metrics = {'ACC': lambda y_true, y_score:accuracy_score(y_true,np.argmax(y_score,axis=1)),
+                           'MCC': lambda y_true, y_score:matthews_corrcoef(y_true,np.argmax(y_score,axis=1)),
+                            'PR-AUC':lambda y_true, y_score: average_precision_score(y_true,np.take_along_axis(y_score,y_true[:,None],axis=1),average='micro')}
                 if n_classes > 2:
-                    metrics['AUC'] = make_scorer(roc_auc_score, multi_class='ovr', average='micro')
+                    metrics['AUC'] = lambda y_true, y_score: roc_auc_score(y_true, y_score,multi_class='ovr', average='micro')
                 else:
-                    metrics['AUC'] = make_scorer(roc_auc_score)
+                    metrics['AUC'] = roc_auc_score
 
-                # clf.fit(X=_X[:,k_best_features],y=_y,)
-                # prediction = clf.predict_proba(X=_X[:,k_best_features])
-                # cv_score = cross_validate(estimator=clf,X=_X[:,k_best_features],y=_y,cv=cv_method,scoring=metrics,error_score='raise')
-                # print(cv_score)
                 X, y = _X[:, k_best_features], _y
+                result = {'fit-time':[],
+                          'inference-time':[],
+                          'ACC':[],
+                          'AUC':[],
+                          'MCC':[],
+                          'PR-AUC':[],
+                          }
                 for train, test in cv_method.split(X,y):
                     X_train=X[train,:]
                     y_train = y[train]
@@ -177,10 +108,15 @@ for dataset in datasets:
                     y_test = y[test]
                     start_time = time.time()
 
-
-                    result = {}
                     clf.fit(X_train, y_train)
                     fit_time = time.time() - start_time
+
                     y_pred = clf.predict_proba(X_test)
-                    test_scores = roc_auc_score(y_true=y_test, y_score=y_pred,multi_class = 'ovr', average = 'macro')
-                    score_time = time.time() - start_time - fit_time
+                    inference_time = time.time() - start_time - fit_time
+
+                    for metric in metrics:
+                        score = metrics[metric](y_test,y_pred)
+                        result[metric].append(score)
+
+
+                    #test_scores = roc_auc_score(y_true=y_test, y_score=y_pred,multi_class = 'ovr', average = 'macro')
